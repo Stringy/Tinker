86144c4cb0f6d9d03efb867f94cca03ed3142e3c
giles <hutton.giles@gmail.com>
2021-10-15 20:29:40 +0100

    Adds proper player model, using sprites from https://szadiart.itch.io/rpg-main-character

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ export (int) var speed = 200
 
 var target = self.position
 var velocity = Vector2()
+var last_direction = Vector2(0, 1)
+var flip_x = false
 
 signal moved(position)
 
@@ -11,7 +13,7 @@ signal moved(position)
 func _ready():
 	pass # Replace with function body.
 	
-func _process(delta):
+func _process(delta: float):
 	var direction = Vector2()
 	if Input.is_action_pressed("forward"):
 		direction.y -= 1.0
@@ -22,13 +24,46 @@ func _process(delta):
 	if Input.is_action_pressed("right"):
 		direction.x += 1.0
 		
-	if Input.is_action_pressed("right_click"):
-		self.look_at(self.get_global_mouse_position())
+	animate_player(direction)
 		
 	self.velocity = direction * self.speed * delta
 	self.position += self.velocity
 	emit_signal("moved", self.position)
 
-func _input(event):
+func _input(event: InputEvent):
 	if event.is_action_pressed("click"):
 		self.target = self.get_global_mouse_position()
+		
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
+	
+func animate_player(direction: Vector2):
+	var flip_x = false
+	if direction != Vector2.ZERO:
+		last_direction = direction
+		var animation = get_animation_direction(last_direction)
+		flip_x = animation == "right"
+		if animation == "left" or animation == "right":
+			animation = "side"
+			
+		animation = animation + "_walk"
+		$Sprite.frames.set_animation_speed(animation, 2 + 8 * direction.length())
+		$Sprite.play(animation)
+	else:
+		var animation = get_animation_direction(last_direction)
+		flip_x = animation == "right"
+		if animation == "left" or animation == "right":
+			animation = "side"
+		animation = animation + "_idle"
+		$Sprite.play(animation)
+	
+	$Sprite.flip_h = flip_x
