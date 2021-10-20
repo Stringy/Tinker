d7f4c7f557dfef46021d9dd4d53a8af342d92f86
giles <hutton.giles@gmail.com>
2021-10-20 11:57:16 +0100

    Adjusted UI to show the most recent change to each of the hunger/health/thirst labels

--- scripts/PlayerUI.gd
+++ scripts/PlayerUI.gd
@@ -3,18 +3,26 @@ extends CanvasLayer
 var hunger = 100 setget set_hunger
 
 func set_hunger(h):
+	_set_stat_ui($Container/Player/Stats/Hunger, "Hunger", hunger, h)
 	hunger = h
-	$Container/Player/Stats/Hunger.text = "Hunger: " + str(self.hunger)
 
 var thirst = 100 setget set_thirst
 
-func set_thirst(h):
-	thirst = h
-	$Container/Player/Stats/Thirst.text = "Thirst: " + str(thirst)
+func set_thirst(t):
+	_set_stat_ui($Container/Player/Stats/Thirst, "Thirst", thirst, t)
+	thirst = t
 
 var health = 100 setget set_health
 
 func set_health(h):
+	_set_stat_ui($Container/Player/Stats/Health, "Health", health, h)
 	health = h
-	$Container/Player/Stats/Health.text = "Health: " + str(health)
 
+
+func _set_stat_ui(label: Label, name: String, old_value: float, new_value: float):
+	var difference = new_value - old_value
+	var signage = ""
+	if difference > 0:
+		signage = "+"
+	
+	label.text = ("%s: %03.2f (%+0.2f)" % [name, new_value, difference])
