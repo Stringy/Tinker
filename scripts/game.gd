06c2350c0302b2cdab5b00746225132b6327b351
giles <hutton.giles@gmail.com>
2021-10-22 09:07:41 +0100

    Refactors significantly to put various parts of the codebase into a more logicalfolder structure. Will probably fiddle around with this in the future, but it makes sense at the moment.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,12 +1,5 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Terrain._generate_world($Player.position)
 	
--- scripts/ui/player/stats.gd
+++ scripts/ui/player/stats.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 
 var hunger = preload("res://resources/player/hunger.tres")
 var thirst = preload("res://resources/player/thirst.tres")
@@ -14,13 +14,13 @@ func _ready():
 	set_health(health.get_value(), 0.0)
 
 func set_hunger(new_hunger, change):
-	_set_stat_ui($Container/Player/Stats/Hunger, "Hunger", new_hunger, change)
+	_set_stat_ui($Hunger, "Hunger", new_hunger, change)
 
 func set_thirst(new_thirst, change):
-	_set_stat_ui($Container/Player/Stats/Thirst, "Thirst", new_thirst, change)
+	_set_stat_ui($Thirst, "Thirst", new_thirst, change)
 
 func set_health(new_health, change):
-	_set_stat_ui($Container/Player/Stats/Health, "Health", new_health, change)
+	_set_stat_ui($Health, "Health", new_health, change)
 
 func _set_stat_ui(label: Label, name: String, new_value, change):
 	label.text = ("%s: %03.2f (%+0.2f)" % [name, new_value, change])
