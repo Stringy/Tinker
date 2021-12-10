d061385513f137a10d4ef97750a78f3f110d3e63
giles <hutton.giles@gmail.com>
2021-12-10 20:09:50 +0000

    Some separation of following behaviours, but probably needs more work

--- scripts/controllers/follow.gd
+++ scripts/controllers/follow.gd
@@ -1,10 +1,36 @@
 extends Controller
 
 export (NodePath) var leaderPath
-export (float) var distance_behind = 50.0
+export (float) var distance_behind = 75.0
 export (float) var slowing_radius = 40.0
+export (float) var stopping_distance = 75.0
+export (float) var separation_distance = 75.0
+export (float) var max_separation = 50.0
 
 # onready var leader = get_node(leaderPath)
+
+func distance(other: Node) -> float:
+    return self.global_position.distance_to(other.global_position)
+
+func separation():
+    var neighbours = 0
+    var force = Vector2()
+    
+    var others = get_tree().get_nodes_in_group(leaderPath)
+    for other in others:
+        if other != self and distance(other) <= separation_distance:
+            force += Vector2(
+                other.global_position.x - self.global_position.x,
+                other.global_position.y - self.global_position.y
+            )
+            neighbours += 1
+    
+    if neighbours != 0:
+        force /= Vector2(neighbours, neighbours)
+        force *= -1
+    
+    force = force.normalized() * max_separation
+    return force
 
 func arrive(target: Vector2):
     var desired = target - self.global_position
@@ -17,6 +43,11 @@ func arrive(target: Vector2):
         desired = desired.normalized() * self.speed
         
     var force = desired - self.get_velocity()
+    force += separation()
+    
+    if force.abs().length() < stopping_distance:
+        force = Vector2.ZERO
+    
     return force
     
 func get_target() -> Vector2:
--- scripts/npc.gd
+++ scripts/npc.gd
@@ -6,15 +6,23 @@ export (NodePath) var leader
 onready var controller = $Controller
 onready var sprites = $AnimatedSprite
 
+# store the old velocity so we don't end up looking down
+# when we stop.
+var old_velocity = Vector2()
+
 func _ready():
-    $Controller.leaderPath = "../" + leader
+    var path = "../" + leader
+    controller.leaderPath = path
+    add_to_group(path)
+    
+func get_velocity():
+    return controller.get_velocity()
 
 func _process(delta):
     var velocity = controller.calculate_movement()
-    print(velocity)
-    if velocity.abs() < Vector2(15.0, 15.0):
-        sprites.animate_direction(velocity, "idle")
-        velocity = Vector2.ZERO
+    if velocity == Vector2.ZERO:
+        sprites.animate_direction(old_velocity, "idle")
     else:
         sprites.animate_direction(velocity, "walk")
+        old_velocity = velocity
     self.move_and_slide(velocity)
